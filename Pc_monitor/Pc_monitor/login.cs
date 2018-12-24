0aea85d52b63f809a4dac264fd5139b5972fe472
ZhangWafer <ZhangWafer@users.noreply.github.com>
2018-12-24 16:56:52 +0800

    Initial commit

---
+++ Pc_monitor/Pc_monitor/login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Pc_monitor;
+
+namespace Pc_monitor
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string account = textBox1.Text;
+            string password = textBox2.Text;
+            DataTable accountTable = SqlHelper.ExecuteDataTable("select * from Cater.PCStaff");
+
+        }
+    }
+}
